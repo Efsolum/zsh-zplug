fdd95f12cd187850c83ee9e904203d0ee582ec37
Matthew Jordan <matthewjordandevops@yandex.com>
2016-08-27 11:38:26 -0400

    Updated package list.
    Removed "hlissner/zsh-autopair".
    Added docker completion with a condition.
    Added a condition to vagrant completion.

--- share/zsh-user-customizations/zplug/Zplug.zsh
+++ share/zsh-user-customizations/zplug/Zplug.zsh
@@ -3,13 +3,14 @@ zplug "zplug/zplug" # ZSH & CLI Package Manager
 
 zplug "plugins/battery", from:oh-my-zsh
 zplug "plugins/git", from:oh-my-zsh
-zplug "hlissner/zsh-autopair"
+zplug "plugins/docker", from:oh-my-zsh, if:"[[ -f $(type docker) ]]"
+zplug "plugins/man", from:oh-my-zsh
 
 # apt-get install libnotify-bin xdotool
 zplug "marzocchi/zsh-notify", if:"[[ -n $DISPLAY ]]"
 
 # Vagrant completions
-zplug "plugins/vagrant", from:oh-my-zsh
+zplug "plugins/vagrant", from:oh-my-zsh, if:"[[ -f $(type vagrant) ]]"
 
 # Directory listings for zsh with git features
 zplug "supercrabtree/k"
